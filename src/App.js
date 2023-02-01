63a0cd7c4614f6f31fa122bc902ed4f854688070
louislallier <louis.lallier@gmail.com>
2023-02-01 15:00:57 +0100

    2nd

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const App = () => {
   const white = "white";
 
   const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
 
   return (
     <div className="App">
@@ -14,13 +16,20 @@ const App = () => {
           <div className="flex">
             <Button
               onClick={() => {
-                setSwitch1(false);
+                setSwitch1(true);
               }}
               text="On"
               backGround={blue}
               textColor={white}
             />
-            <Button text="OFF" backGround={white} textColor={blue} />
+            <Button
+              onClick={() => {
+                setSwitch1(false);
+              }}
+              text="OFF"
+              backGround={white}
+              textColor={blue}
+            />
           </div>
         ) : (
           <div className="flex">
@@ -32,7 +41,96 @@ const App = () => {
               backGround={white}
               textColor={blue}
             />
-            <Button text="OFF" backGround={blue} textColor={white} />
+            <Button
+              onClick={() => {
+                setSwitch1(false);
+              }}
+              text="OFF"
+              backGround={blue}
+              textColor={white}
+            />
+          </div>
+        )}
+      </div>
+      <div>
+        {switch2 ? (
+          <div className="flex">
+            <Button
+              onClick={() => {
+                setSwitch2(true);
+              }}
+              text="On"
+              backGround={blue}
+              textColor={white}
+            />
+            <Button
+              onClick={() => {
+                setSwitch2(false);
+              }}
+              text="OFF"
+              backGround={white}
+              textColor={blue}
+            />
+          </div>
+        ) : (
+          <div className="flex">
+            <Button
+              onClick={() => {
+                setSwitch2(true);
+              }}
+              text="On"
+              backGround={white}
+              textColor={blue}
+            />
+            <Button
+              onClick={() => {
+                setSwitch2(false);
+              }}
+              text="OFF"
+              backGround={blue}
+              textColor={white}
+            />
+          </div>
+        )}
+      </div>
+      <div>
+        {switch3 ? (
+          <div className="flex">
+            <Button
+              onClick={() => {
+                setSwitch3(true);
+              }}
+              text="On"
+              backGround={blue}
+              textColor={white}
+            />
+            <Button
+              onClick={() => {
+                setSwitch3(false);
+              }}
+              text="OFF"
+              backGround={white}
+              textColor={blue}
+            />
+          </div>
+        ) : (
+          <div className="flex">
+            <Button
+              onClick={() => {
+                setSwitch3(true);
+              }}
+              text="On"
+              backGround={white}
+              textColor={blue}
+            />
+            <Button
+              onClick={() => {
+                setSwitch3(false);
+              }}
+              text="OFF"
+              backGround={blue}
+              textColor={white}
+            />
           </div>
         )}
       </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
-const Button = ({ textColor, backGround, text }) => {
+const Button = ({ textColor, backGround, text, onClick }) => {
   return (
     <div>
       <button
+        onClick={onClick}
         className="m-4 w-20 rounded-md border border-blue-400 py-3 px-4"
         style={{ backgroundColor: backGround, color: textColor }}
       >
